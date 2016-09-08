79bc2edbdc758c818dec72f0e0e3e6623292d867
unnmd <vit.ovchinnikov@gmail.com>
2016-09-08 21:34:32 +0300

    add readme

--- CheckIP.bat
+++ CheckIP.bat
@@ -6,7 +6,7 @@ REM ####END SYSTEM SET####
 
 
 REM ####START SCRIPT SET####
-set ip=192.168.1
+set net=192.168.1
 set int=0xc
 set start=1
 set end=255
@@ -15,25 +15,24 @@ REM ####END SCRIPT SET####
 set /a "x = %start%"
 :while1
 if %x% LSS %end% (
-        ping %ip%.%x% -n 1 -w 1000 | find "TTL=" >nul
+        ping %net%.%x% -n 1 -w 1000 | find "TTL=" >nul
 		if errorlevel 1 ( 
-				echo host %ip%.%x% not reachable >> down.txt
-				arp -a %ip%.%x% | find "%int%" >nul
+				echo host %net%.%x% not reachable >> down.txt
+				arp -a %net%.%x% | find "%int%" >nul
 				if errorlevel 1 ( 
     					echo no record
 				) else (
-					arp -a %ip%.%x% >> down.txt
+					arp -a %net%.%x% >> down.txt
 				)
 		) else (
-			echo host %ip%.%x% reachable >> up.txt
-			nmap --open %ip%.%x%  | find "PORT     STATE SERVICE" >>1.txt
+			echo host %net%.%x% reachable >> up.txt
 		)
 	cls        
 	set /a "x = x + 1"
 	echo ====================
 	echo ^|     CheckIP      ^|
 	echo ====================
-	echo      %ip%.%x%  
+	echo      %net%.%x%  
 	echo ====================
 	echo ^|      v0.1        ^|
     echo ====================
